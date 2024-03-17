6b8d2a5f768ca90ede0c610d8a5d50f7c2aee862
Denilson Guedes <denilsondavid.me@gmail.com>
2024-03-17 10:24:36 -0700

    created navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -3,13 +3,24 @@ import React from 'react';
 const NavBar: React.FC = () => {
     return (
         <div className="navbar bg-base-100">
-            <div className="flex-none">
-                <button className="btn btn-square btn-ghost">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                </button>
-            </div>
-            <div className="flex-1">
-                <a className="btn btn-ghost text-xl">Barakat</a>
+            <div className="drawer">
+                <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content flex items-center">
+                    <label htmlFor="my-drawer" className="btn btn-ghost drawer-button">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
+                    </label>
+                    <div className="flex-1">
+                        <a className="btn btn-ghost text-xl">Barakat</a>
+                    </div>
+                </div>
+                <div className="drawer-side">
+                    <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu p-4 w-80 min-h-full bg-white text-base-content">
+                        <li><a>Sidebar Item 1</a></li>
+                        <li><a>Sidebar Item 2</a></li>
+
+                    </ul>
+                </div>
             </div>
             <div className="flex-none">
                 <button className="btn btn-square btn-ghost">
