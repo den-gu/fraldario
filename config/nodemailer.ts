11b6b478d3b7fe21894974703eb24940498dcfe8
Denilson Guedes <denilson.guedes@xipefo.com>
2024-09-30 16:41:31 +0200

    set nodemailer auth values

--- config/nodemailer.ts
+++ config/nodemailer.ts
@@ -1,7 +1,7 @@
 import nodemailer from "nodemailer"
 
-// const email = process.env.EMAIL;
-// const password = process.env.EMAIL_PASS;
+const email = process.env.EMAIL;
+const password = process.env.EMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
@@ -9,12 +9,12 @@ export const transporter = nodemailer.createTransport({
     port: 587,
     secure: true, // Use `true` for port 465, `false` for all other ports
     auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS,
+      user: email,
+      pass: password,
     },
   });
 
 export const mailOptions = {
-    from: process.env.EMAIL,
-    to: process.env.EMAIL
+    from: email,
+    to: email
 }
