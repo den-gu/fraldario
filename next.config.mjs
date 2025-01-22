cdea91fa0f24671f9d4f969d06fd39035e365c69
Denilson Guedes <57498621+den-gu@users.noreply.github.com>
2025-01-22 15:18:35 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -13,7 +13,7 @@ const nextConfig = {
             },
           {
             protocol: 'https',
-            hostname: 'i.postimg.cc',
+            hostname: 'raw.githubusercontent.com',
             port: '',
           },
         ],
