307b72c4f76537bc2f2b1645934cab5cd362a446
Denilson Guedes <57498621+den-gu@users.noreply.github.com>
2025-03-23 17:07:18 +0200

    Update get-report.tsx

--- components/get-report.tsx
+++ components/get-report.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { zodResolver } from "@hookform/resolvers/zod"
 // import { CalendarIcon } from "lucide-react"
 import { useForm } from "react-hook-form"
